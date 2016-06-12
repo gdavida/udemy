687d61ace73876b97eb5bc02ea1e8cb032a011d7
Davida Gaffney <gdavida@gmail.com>
2016-06-12 11:26:02 -0500

    welcome pages routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/contact'
+  get 'welcome/features'
+  get 'welcome/faq'
+  get 'welcome/pricing'
   resources :invoices
   
-  root to: 'invoices#index'
+  root to: 'welcome#index'
+
+  
+  
+
 end
