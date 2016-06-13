335bfc2653619d3ebf5a8a7ccbfa48f10023a649
Davida Gaffney <gdavida@gmail.com>
2016-06-12 22:01:12 -0500

    csv uploader added and working wooohooooooo

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  resources :companies
   get 'welcome/index'
   get 'welcome/about'
   get 'welcome/contact'
   get 'welcome/features'
   get 'welcome/faq'
   get 'welcome/pricing'
-  resources :invoices
   
+  resources :invoices
+  resources :companies do
+  	collection { post :import}
+  end
+
   root to: 'welcome#index'
 
   
